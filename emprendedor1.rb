2e4e4aaa16f7182b6edbb379789d52e8bdf08c8b
oscarpw <oscar.castillo@primeraweb.cl>
2019-05-07 21:45:48 -0400

    emprendedor1 listo

---
+++ emprendedor1.rb
@@ -0,0 +1,14 @@
+ingreso_datos=ARGV
+precio_venta=ingreso_datos[0].to_f
+usuarios=ingreso_datos[1].to_f
+gastos=ingreso_datos[2].to_f
+
+utilidades=((precio_venta*usuarios)-gastos)
+
+if utilidades > 0
+    impuestos=utilidades*0.35
+    utilidades=utilidades*0.65
+    puts "Las utilidades son #{utilidades}, aplicando un impuesto total de #{impuestos}"
+else
+    puts "Registra una pérdida de #{utilidades}"
+end
